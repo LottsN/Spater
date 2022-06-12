bdb6addd57e0327eb249fe34e41832bddaa2b9c5
Nikita Lotts <n.lotz@yandex.ru>
2022-06-12 11:31:43 +0300

    Add login and register functions
    Realeazed login and register functions, create service, controllers and view, but its no data pass between them all

---
+++ rrt/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rrt.Controllers
+{
+    public class AuthController
+    {
+        ILoginView _loginView;
+        IRegisterView _registerView;
+        IAuthService _authService;
+
+        public AuthController(ILoginView loginView, IRegisterView registerView, IAuthService authService)
+        {
+            _loginView = loginView;
+            _registerView = registerView;
+            _authService = authService;
+            loginView.SetController(this);
+            registerView.SetController(this);
+        }
+    }
+}
---
+++ rrt/Presenters/Views/ILoginView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rrt.Presenters.Views
+{
+    public interface ILoginView
+    {
+        void SetController(AuthController controller);
+        bool Login(string Email, string Password);
+        void ShowError();
+
+        string Email { get; set; }
+        string Password { get; set; }
+    }
+}
